cede3aa71bef5c816a475214df39bcd65c926154
Igor-Siatkovsky <igorsiatkovsky@gmail.com>
2018-09-25 14:51:19 +0300

    update currenciesPriceArr

--- js/currenciesDB.js
+++ js/currenciesDB.js
@@ -24,6 +24,23 @@ var allCurrenciesArr = allCurrenciesRaw.split('\n');
 var allCurrenciesHtmlFirstColumn = '';
 var allCurrenciesHtmlSecondColumn = '';
 
+/* var currenciesPrice = {
+    'USDT': 1,
+    'BTC': 6465.98,
+    'ETH': 228.61,
+    'BCH': 508.43,
+    'LTC': 55.73,
+    'RPL': 0.29,
+    'XMR': 114.81,
+    'MKR': 391.68,
+    'DASH': 172.90,
+    'XRP': 0.29,
+} */
+
+read.open('GET', 'coins_price.txt', false);
+read.send();
+var currenciesPrice = JSON.parse(read.responseText);
+
 allCurrenciesArr.map(item => {
     var coinTitle = item.split('-')[0].trim();
     var coinShort = item.split('-')[1].trim();
@@ -129,19 +146,6 @@ var wallets = {
             'DASH': 0.00,
             'XRP': 0.00,
         } */
-}
-
-var currenciesPrice = {
-    'USDT': 1,
-    'BTC': 6465.98,
-    'ETH': 228.61,
-    'BCH': 508.43,
-    'LTC': 55.73,
-    'RPL': 0.29,
-    'XMR': 114.81,
-    'MKR': 391.68,
-    'DASH': 172.90,
-    'XRP': 0.29,
 }
 
 var eachBalance = {};
