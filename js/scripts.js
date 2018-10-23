ad00dda7e8774c5ed329871e7d2eb2075538f3f8
Igor-Siatkovsky <igorsiatkovsky@gmail.com>
2018-10-24 00:18:33 +0300

    check window height before append fancybox

--- js/scripts.js
+++ js/scripts.js
@@ -1573,25 +1573,30 @@ $(function () {
 					touch: false,
 					afterShow: function (instance, current) {
 						var fancybox_body = $('.fancybox-container')[0];
-					//	$('.main-cols__right')[0].append(fancybox_body);
-						$('.b-graph')[0].append(fancybox_body);
-						// $('.main-cols__right .fancybox-container')
-						// 	.css({
-						// 		"width": "100%",
-						// 		"height": "100%",
-						// 		"display": "block",
-						// 		"position": "absolute"
-						// 	})
-						// 	.css("display", "block");
-
-						$('.b-graph .fancybox-container')
-							.css({
-								"width": "100%",
-								"height": "100%",
-								"display": "block",
-								"position": "absolute"
-							})
-							.css("display", "block");
+						// show in full window height
+						if ($(window).height() < 900){
+							$('.main-cols__right')[0].append(fancybox_body);
+							$('.main-cols__right .fancybox-container')
+								.css({
+									"width": "100%",
+									"height": "100%",
+									"display": "block",
+									"position": "absolute"
+								})
+								.css("display", "block");
+						}
+						// show in chart
+						else{
+							$('.b-graph')[0].append(fancybox_body);
+							$('.b-graph .fancybox-container')
+								.css({
+									"width": "100%",
+									"height": "100%",
+									"display": "block",
+									"position": "absolute"
+								})
+								.css("display", "block");
+						}
 
 						$('#transaction-popup .transaction-form__input').keyup(function () {
 							if ($(this).val() == '' || $(this).hasClass('inactive')) {
