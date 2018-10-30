cc410434a8bd43cf7ec1da7deb55314a3dc7f81d
Igor-Siatkovsky <igorsiatkovsky@gmail.com>
2018-10-30 18:01:53 +0300

    remove unused part of code

--- js/scripts.js
+++ js/scripts.js
@@ -674,31 +674,6 @@ $(function () {
 		redrawMainChart();
 	});
 
-	// $('.b-graph .c-block').mousemove(function (e) {
-	// 	var x = e.pageX - $('.b-graph').offset().left;
-	// 	if ($('.b-graph').width() - x < 10) {
-	// 		if (!$('.graph-prices').hasClass('open')) {
-	// 			$('.graph-prices').addClass('open');
-	// 			$('#mainChart').css('width', 'calc(100% + 6px)');
-	// 			$('.b-graph__controls').addClass('shifted');
-	// 			$('.b-graph__controls .graph-prices__controls__btn__open').removeClass('open');
-	// 			redrawMainChart();
-	// 		}
-	// 	} else {
-	// 		if ($('.b-graph').width() - x > 300) {
-	// 			if ($('.graph-prices').hasClass('open') && !$('.graph-prices').hasClass('noClose')) {
-	// 				$('.graph-prices').removeClass('open');
-	// 				$('#mainChart').css('width', 'calc(100% - 4px)');
-	// 				$('.b-graph__controls').removeClass('shifted');
-	// 				$('.b-graph__controls .graph-prices__controls__btn__open').addClass('open');
-	// 				redrawMainChart();
-	// 				// reset prices scroll
-	// 				$('.scrollbar-arrows').scrollTop(0);
-	// 			}
-	// 		}
-	// 	}
-	// });
-
 	$('body').mousemove(function (e) {
 		var x = e.pageX - $('.b-graph').offset().left;
 		if ($('.b-graph').width() - x > 220) {
@@ -751,10 +726,6 @@ $(function () {
 	/*---------------------------------------------------*/
 	/* ADVANCED account-js-menu */
 	/*---------------------------------------------------*/
-
-	// $('.advanced .accounts-diagram-wrap').click(function () {
-	// 	$('.main-cols__right-bottom .portfolio-menu__item').eq(2).trigger('click');
-	// });
 
 	$('.advanced .js-account-stats .portfolio-drop .menu-dropdown .menu-dropdown__item').on('click', function (event) {
 		event.stopPropagation();
@@ -823,16 +794,6 @@ $(function () {
 	/*---------------------------------------------------*/
 	/* functions for change theme */
 	/*---------------------------------------------------*/
-
-	// $('#switch-theme').change(function () {
-	// 	var darkTheme = $('#switch-theme:checked').length;
-	// 	if (darkTheme) {
-	// 		$('body').addClass('dark-theme');
-	// 	} else {
-	// 		$('body').removeClass('dark-theme');
-	// 	}
-	// 	changeTheme();
-	// });
 
 	function changeTheme() {
 		if ($('body').hasClass('dark-theme')) {
@@ -1042,10 +1003,6 @@ $(function () {
 	/* chat events */
 	/*---------------------------------------------------*/
 
-	// $('.chat-head__back').click(function () {
-	// 	$('.main-cols__left-top .d-flex .c-block__col').toggleClass('hidden');
-	// });
-
 	$('.chats-list__item').click(function () {
 		if (localStorage.getItem('telegramAuth') == 'true'){
 			var chatName = $(this).find('.chats-list__name').text();
@@ -1123,31 +1080,26 @@ $(function () {
 						gData = gDataTwoHour;
 						range_index = 0;
 						if (current_range == '1H') isRedraw = true;
-						//console.log('gDataTwoHour');
 					} else if ((data.TimeTo - data.TimeFrom) * 1000 / data.Data.length <= 1.2 * range_options[1].interval) {
 						gDataDay = [];
 						gData = gDataDay;
 						range_index = 1;
 						if (current_range == '1D') isRedraw = true;
-						//console.log('gDataDay');
 					} else if ((data.TimeTo - data.TimeFrom) * 1000 / data.Data.length <= 1.2 * range_options[2].interval) {
 						gDataWeek = [];
 						gData = gDataWeek;
 						range_index = 2;
 						if (current_range == '1W') isRedraw = true;
-						//console.log('gDataWeek');
 					} else if ((data.TimeTo - data.TimeFrom) * 1000 / data.Data.length <= 1.2 * range_options[3].interval) {
 						gDataMonth = [];
 						gData = gDataMonth;
 						range_index = 3;
 						if (current_range == '1M') isRedraw = true;
-						//console.log('gDataMonth');
 					} else if ((data.TimeTo - data.TimeFrom) * 1000 / data.Data.length <= 1.2 * range_options[4].interval) {
 						gDataYear = [];
 						gData = gDataYear;
 						range_index = 4;
 						if (current_range == '1Y') isRedraw = true;
-						//console.log('gDataYear');
 					}
 
 					var prev_value = [null, null, null, null, null, null];
@@ -1211,7 +1163,6 @@ $(function () {
 							}
 							gData.push(one_graph);
 						}
-						//console.log(gData);
 						if (isRedraw) {
 
 							mainChartObj.series[0].setData(gData[0].prices);
@@ -2518,7 +2469,7 @@ $(function () {
 						})
 						.css("display", "block");
 					if (popupSrcDiv == "confirm-withdrawal-request") {
-						console.log("populating next screen for demo!")
+						//console.log("populating next screen for demo!")
 						try {
 							window.current_wallet_session.withdraw_address = $("#withdrawal-address").val()
 
@@ -2526,9 +2477,9 @@ $(function () {
 							$("#confirm-return-address").html(window.current_wallet_session.withdraw_address)
 							$("#confirm-return-currency").html(window.current_wallet_session.currency)
 
-							console.log("done, data was " + JSON.stringify(window.current_wallet_session))
+							//console.log("done, data was " + JSON.stringify(window.current_wallet_session))
 						} catch (x) {
-							console.log("ERROR populating next screen")
+							//console.log("ERROR populating next screen")
 						}
 
 					}
@@ -2536,10 +2487,6 @@ $(function () {
 				beforeShow: function () {
 					$('.fancybox-container').css("display", "none");
 				},
-				beforeClose: function () {
-					//$('.exch-form').removeClass('progress');
-					//$('.exch-head').toggleClass('open');
-				}
 			}
 		});
 	});
